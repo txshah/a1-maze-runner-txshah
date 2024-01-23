fc1895f263655de3350972d1fad07b7a478e7900
Tvesha Shah <tveshashah13@gmail.com>
2024-01-22 19:15:50 -0500

    close #9

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/path_check.java
@@ -0,0 +1,14 @@
+
+package ca.mcmaster.se2aa4.mazerunner;
+
+public class path_check{
+    //confirm that the tag is -p 
+    public int fac2can(){//method will convert factorial input into can inptu 
+        return " No conversion yet";
+    }
+
+    public boolean check(){
+        //check if numbers work 
+        //call path_find if wrong 
+    }
+}
