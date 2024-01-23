40825660b81dea3bf682f5101a0e1bd1c12d8996
Tvesha Shah <tveshashah13@gmail.com>
2024-01-23 16:43:25 -0500

    close #4

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -19,25 +19,54 @@ public class Main {
     private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
+
         logger.info("** Starting Maze Runner");
         
+        try{
+            Configuration config = configure(args);
+            //System.out.println(config);
+            logger.info("**** Computing path");
+
+        }catch(Exception e) {
+            logger.error("/!\\ An error has occured /!\\");
+            logger.error("PATH NOT COMPUTED");
+        }
+
+        logger.info("** End of MazeRunner");
+    }
+
+    private static Configuration configure(String[] args) throws ParseException{
         //command line options 
         Options options = new Options();
         options.addOption("i", true, "MAZE_FILE");
         options.addOption("p", true, "PATH_SEQUENCE");
+        
+        CommandLineParser parser = new DefaultParser();//parser to get values from command line 
+        CommandLine cmd = parser.parse(options, args);
 
-        CommandLineParser parser = new DefaultParser();//parser 
+        String MAZE_FILE = cmd.getOptionValue("i");
+        logger.info("**** Reading the maze from file " + MAZE_FILE);
 
-        try {
-            
-            CommandLine cmd = parser.parse(options, args);
-            String MAZE_FILE = cmd.getOptionValue("i");
-            String PATH_SEQUENCE = cmd.getOptionValue("p");//will add command for this after 
 
-            logger.info("**** Reading the maze from file " + MAZE_FILE);
+        String PATH_SEQUENCE = cmd.getOptionValue("p");
+
+        return new Configuration(MAZE_FILE, PATH_SEQUENCE);
+    }
+
+    private record Configuration(String MAZE_FILE, String PATH_SEQUENCE) {//use to ensure tags are being used 
+        Configuration{
+            if (MAZE_FILE ==null){
+                throw new IllegalArgumentException("Please input a valid tag (-i) to run program. Remeber to also use tag -p if you would like to test your own path");
+            }
+        }
+    }
+
+}
+
+    /*
+code for printing maze 
             BufferedReader reader = new BufferedReader(new FileReader(MAZE_FILE));
-           
-            String line;
+            String line; 
             while ((line = reader.readLine()) != null) {
                 for (int idx = 0; idx < line.length(); idx++) {
                     if (line.charAt(idx) == '#') {
@@ -48,11 +77,4 @@ public class Main {
                 }
                 System.out.print(System.lineSeparator());
             }
-        } catch(Exception e) {
-            logger.error("/!\\ An error has occured /!\\");
-        }
-        logger.info("**** Computing path");
-        logger.info("PATH NOT COMPUTED");
-        logger.info("** End of MazeRunner");
-    }
-}
+     */
